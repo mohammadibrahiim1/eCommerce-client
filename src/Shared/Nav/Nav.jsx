afe71b15bad0c56586ac194585304ae5b5fd77ae
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-12-11 02:31:29 +0600

    add dropdown menu

--- src/Shared/Nav/Nav.jsx
+++ src/Shared/Nav/Nav.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/jsx-no-undef */
-import { Button, Navbar, TextInput } from "keep-react";
+import { Button, Dropdown, Navbar, TextInput } from "keep-react";
 import {
+  CaretRight,
+  Gear,
   Heart,
   MagnifyingGlass,
+  Money,
   Phone,
   ShoppingCart,
+  SignOut,
+  SquaresFour,
   User,
 } from "phosphor-react";
 
@@ -21,15 +26,47 @@ const Nav = () => {
             tag="ul"
             className="lg:flex hidden items-center justify-between gap-8 "
           >
+            <Dropdown
+              className="px-5"
+              label="All Category"
+              size="sm"
+              type="outlineGray"
+              dismissOnClick={true}
+            >
+              <Dropdown.Item icon={<SquaresFour size={20} color="#5E718D" />}>
+                Dashboard
+                <span className="ml-auto">
+                  <CaretRight size={20} color="#5E718D" />
+                </span>
+              </Dropdown.Item>
+              <Dropdown.Item icon={<Gear size={20} color="#5E718D" />}>
+                Settings
+                <span className="ml-auto">
+                  <CaretRight size={20} color="#5E718D" />
+                </span>
+              </Dropdown.Item>
+              <Dropdown.Item icon={<Money size={20} color="#5E718D" />}>
+                Earnings
+                <span className="ml-auto">
+                  <CaretRight size={20} color="#5E718D" />
+                </span>
+              </Dropdown.Item>
+              <Dropdown.Item icon={<SignOut size={20} color="#5E718D" />}>
+                Sign out
+                <span className="ml-auto">
+                  <CaretRight size={20} color="#5E718D" />
+                </span>
+              </Dropdown.Item>
+            </Dropdown>
+
             <TextInput
               id="#id-10"
               placeholder="Search anything"
               color="gray"
-              sizing="sm"
+              sizing="md"
               type="text"
               addon={<MagnifyingGlass size={20} color="#5E718D" />}
               addonPosition="left"
-              className="w-full"
             />
           </Navbar.Container>
 
