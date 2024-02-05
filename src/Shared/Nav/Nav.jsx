a5605a98e6791e58d21d54c584ceded394448a0c
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2024-02-05 21:07:14 +0600

    add social icon in navbar

--- src/Shared/Nav/Nav.jsx
+++ src/Shared/Nav/Nav.jsx
@@ -1,12 +1,91 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { FaHeart, FaHome, FaUser } from "react-icons/fa";
+import {
+  FaAngleDown,
+  FaHeart,
+  FaHome,
+  FaInstagram,
+  FaTwitter,
+  FaUser,
+} from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { FaFacebook } from "react-icons/fa";
 
 const Nav = () => {
   return (
     <div className="bg-[#1B6392]">
+      <div className="flex justify-between items-center gap-4 w-[1320px] mx-auto py-5">
+        <h3 className="text-[#FFFFFF] font-semibold text-[14px] ">
+          Welcome to Clicon online eCommerce store.
+        </h3>
+        <div className="flex justify-center items-center gap-3">
+          <h1 className="text-[#FFFFFF] font-semibold text-[14px]">
+            Follow us on :{" "}
+          </h1>
+          <div className="flex  items-center  gap-3">
+            <Link to={"https://www.facebook.com/"}>
+              <FaFacebook className=" text-white" />
+            </Link>
+            <Link className=" text-white" to={"https://www.facebook.com/"}>
+              <FaInstagram />
+            </Link>
+            <Link className=" text-white" to={"https://www.facebook.com/"}>
+              <FaTwitter />
+            </Link>
+
+            <div>
+              <div className="flex justify-end flex-1 px-2">
+                <div className="flex items-center ">
+                  <div className="dropdown dropdown-end">
+                    <div
+                      tabIndex={0}
+                      role="button"
+                      className="btn btn-xs btn-ghost rounded-btn text-white gap-1"
+                    >
+                      <span>USD</span>
+                      <FaAngleDown />
+                    </div>
+                    <ul
+                      tabIndex={0}
+                      className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+                    >
+                      <li>
+                        <a>Item 1</a>
+                      </li>
+                      <li>
+                        <a>Item 2</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="dropdown dropdown-end">
+                    <div
+                      tabIndex={0}
+                      role="button"
+                      className="btn btn-xs btn-ghost rounded-btn text-white gap-1"
+                    >
+                      <span>Language</span>
+                      <FaAngleDown />
+                    </div>
+                    <ul
+                      tabIndex={0}
+                      className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+                    >
+                      <li>
+                        <a>Item 1</a>
+                      </li>
+                      <li>
+                        <a>Item 2</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className="text-gray-600" />
       <div>
         <div className="navbar w-[1320px] mx-auto pt-5">
           <div className="navbar-start">
@@ -60,7 +139,7 @@ const Nav = () => {
           </div>
           <div>
             <input
-              className="border w-[646px] h-[42px] p-2 rounded focus-outline:none "
+              className="border w-[646px] h-[42px] p-2 rounded focus:outline-none "
               type="search"
               name="search"
               placeholder="search..."
